7c5ab7068b9be922af19a37cfc68aa5441a2ccc9
Marcos Vinicius <marcos.viniciuss.2102@gmail.com>
2024-12-01 09:39:05 -0300

    Salvando arquivos para alterar a branch

--- GerenciadorDeTarefas/obj/Debug/net8.0/GerenciadorDeTarefas.AssemblyInfo.cs
+++ GerenciadorDeTarefas/obj/Debug/net8.0/GerenciadorDeTarefas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciadorDeTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff4c4c430ce5e8c011c3b8d69527c74cceade86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f7221df798dddf70ab43320a52e049dde3d6665")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciadorDeTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciadorDeTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GerenciadorDeTarefas/obj/Release/net8.0/GerenciadorDeTarefas.AssemblyInfo.cs
+++ GerenciadorDeTarefas/obj/Release/net8.0/GerenciadorDeTarefas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciadorDeTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f7221df798dddf70ab43320a52e049dde3d6665")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciadorDeTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciadorDeTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
